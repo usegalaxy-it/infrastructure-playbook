533e98a99d36b99eb8c918bd6b122451ea5bcd17
pmandreoli <pietro.mandreoli@gmail.com>
2021-11-03 15:50:40 +0000

    add mod and nginx upload store

---
+++ files/nginx_upload/nginx_upload.sh
@@ -0,0 +1,19 @@
+!#/bin/bash
+#wget https://nginx.org/download/nginx-$1.tar.gz
+nginx_url="https://nginx.org/download/nginx-$1.tar.gz"
+nginx_module_url="https://github.com/fdintino/nginx-upload-module/archive/$2.tar.gz"
+version_nginx=$1
+version_module=$2
+mkdir nginx_temp
+cd nginx_temp
+wget -nc $nginx_url
+wget -nc $nginx_module_url
+tar -xvf $(basename $nginx_url)
+tar -xvf $( basename $nginx_module_url)
+cd nginx-$version_nginx
+nginx -V |& awk -v var=$version_module -F "configure arguments:" '{ print $2 " --add-dynamic-module=../nginx-upload-module-"var }' | xargs ./configure
+make modules
+
+sudo cp objs/ngx_http_upload_module.so  /usr/lib64/nginx/modules
+cd -
+cd .. && rm -rf nginx_temp
---
+++ roles/usegalaxy_eu.fs_maintenance/README.md
@@ -0,0 +1,20 @@
+ansible-fs-maintenance
+=======
+
+Ansible role for deploying and configuring some common Galaxy file system maintenance routines in a RedHat/CentOS system.
+
+Requirements
+------------
+Ansible >= 2.11
+
+Role Variables
+--------------
+See [defaults/main.yml](defaults/main.yml).
+     
+License
+-------
+GPLv3
+
+Author Information
+------------------
+[Galaxy Europe](https://galaxyproject.eu)
