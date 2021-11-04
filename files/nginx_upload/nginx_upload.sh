8c1eb679675fc3962517fd480cf4c4b0dac4f0df
pmandreoli <pietro.mandreoli@gmail.com>
2021-11-04 15:00:19 +0000

    clean nginx conf, compile nginx to add upload store

--- files/nginx_upload/nginx_upload.sh
+++ files/nginx_upload/nginx_upload.sh
@@ -1,4 +1,4 @@
-!#/bin/bash
+!#/usr/bin/bash
 #wget https://nginx.org/download/nginx-$1.tar.gz
 nginx_url="https://nginx.org/download/nginx-$1.tar.gz"
 nginx_module_url="https://github.com/fdintino/nginx-upload-module/archive/$2.tar.gz"
@@ -14,6 +14,6 @@ cd nginx-$version_nginx
 nginx -V |& awk -v var=$version_module -F "configure arguments:" '{ print $2 " --add-dynamic-module=../nginx-upload-module-"var }' | xargs ./configure
 make modules
 
-sudo cp objs/ngx_http_upload_module.so  /usr/lib64/nginx/modules
+sudo cp objs/ngx_http_upload_module.so  /usr/lib64/nginx/modules/
 cd -
 cd .. && rm -rf nginx_temp
